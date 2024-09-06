91b216f01583f6aed64bb6736435657b95e1eb71
Dipayan Kar <98595761+R3d-Dr4g0n@users.noreply.github.com>
2024-09-06 23:51:20 +0530

    Create Missing_number.java

---
+++ Missing_number.java
@@ -0,0 +1,11 @@
+// Missing Number in an Array
+
+public int missingNumber(int[] nums) {
+    int n = nums.length;
+    int totalSum = n * (n + 1) / 2;
+    int arraySum = 0;
+    for (int num : nums) {
+        arraySum += num;
+    }
+    return totalSum - arraySum;
+}
