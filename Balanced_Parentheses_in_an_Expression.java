cd0c0fe7538158c20da22f58c30cb50065889046
Dipayan Kar <98595761+R3d-Dr4g0n@users.noreply.github.com>
2024-09-06 23:48:54 +0530

    Create Balanced_Parentheses_in_an_Expression.java

---
+++ Balanced_Parentheses_in_an_Expression.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public boolean isBalanced(String expression) {
+    Stack<Character> stack = new Stack<>();
+    for (char c : expression.toCharArray()) {
+        if (c == '(' || c == '{' || c == '[') {
+            stack.push(c);
+        } else if (c == ')' || c == '}' || c == ']') {
+            if (stack.isEmpty()) return false;
+            char top = stack.pop();
+            if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+    return stack.isEmpty();
+}
